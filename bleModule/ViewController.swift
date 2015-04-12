422c62e966cb91949ed547c1bfa4279abe0f1b2a
Thomas Brittain <design@ubermentis.com>
2015-04-12 09:22:55 -0500

    Initial Commit

---
+++ bleModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bleModule
+//
+//  Created by Casey Brittain on 4/12/15.
+//  Copyright (c) 2015 Casey Brittain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
